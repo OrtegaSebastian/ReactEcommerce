bff8fca7d0df0da757c70be69ef19960f08d4639
sebaortega <ortegamolarosebastian@gmail.com>
2022-06-07 19:36:30 -0300

    ItemFuncionError

---
+++ src/components/ItemList/item.jsx
@@ -0,0 +1,10 @@
+import {setTime} from "react";
+
+function Item({ item }) {
+    // Desarrolla la vista de un ítem donde item es de tipo
+    // { id, title, price, pictureUrl }
+//    }
+   
+function Item(){
+
+}
